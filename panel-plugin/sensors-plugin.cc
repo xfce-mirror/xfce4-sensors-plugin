480265ea7b3e3ce028a5b35e6f5f2adef290a04e
Bobby Rong <rjl931189261@126.com>
2025-03-18 23:50:50 +0800

    Add meson build

--- panel-plugin/sensors-plugin.cc
+++ panel-plugin/sensors-plugin.cc
@@ -29,6 +29,9 @@
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
+#ifdef HAVE_XFCE_REVISION_H
+#include "xfce-revision.h"
+#endif
 
 #include <gio/gio.h>
 #include <glib.h>
@@ -2065,7 +2068,7 @@ sensors_show_about (XfcePanelPlugin *plugin)
    gtk_show_about_dialog (NULL,
       "logo", icon,
       "license", xfce_get_license_text (XFCE_LICENSE_TEXT_GPL),
-      "version", PACKAGE_VERSION,
+      "version", VERSION_FULL,
       "program-name", PACKAGE_NAME,
       "comments", _("Show sensor values from LM sensors, ACPI, hard disks, NVIDIA"),
       "website", "https://docs.xfce.org/panel-plugins/xfce4-sensors-plugin",
